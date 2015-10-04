4cb50bb9afd21f6422067f0dac207bad6881cb16
Juan Toledo Carrasco <juantoledo@gmail.com>
2015-10-04 12:12:50 -0300

    Added view

--- sql/weather_last_hour.sql
+++ sql/weather_last_hour.sql
@@ -1,11 +1,13 @@
 
 create or replace view weather_last_hour as
 select 
-getwinddir(wind_direction) as wind_dir,
-round(avg(wind_speed)) as wind_speed,
+case when round(avg(wind_speed)) < 5 then '-' else getwinddir(wind_direction) end as wind_dir,
+case when round(avg(wind_speed)) < 5 then 0 else round(avg(wind_speed)) end as wind_speed,
 round(avg(humidity)) as humidity,
 round(avg(temperature)) as temperature,
-round(avg(pressure)) as pressure
+round(avg(pressure)) as pressure,
+case when round(avg(wind_speed)) < 5 then 0 else round(max(wind_speed)) end as gust
+
 from 
 weather_data 
 
@@ -15,3 +17,21 @@ group by wind_direction
 order by count(wind_direction) desc limit 1;
 
 
+
+
+create or replace view weather_last_ten_minutes as
+select 
+case when round(avg(wind_speed)) < 5 then '-' else getwinddir(wind_direction) end as wind_dir,
+case when round(avg(wind_speed)) < 5 then 0 else round(avg(wind_speed)) end as wind_speed,
+round(avg(humidity)) as humidity,
+round(avg(temperature)) as temperature,
+round(avg(pressure)) as pressure,
+case when round(avg(wind_speed)) < 5 then 0 else round(max(wind_speed)) end as gust
+
+from 
+weather_data 
+
+
+where datetime > now() - interval '10 minutes'
+group by wind_direction
+order by count(wind_direction) desc limit 1;
